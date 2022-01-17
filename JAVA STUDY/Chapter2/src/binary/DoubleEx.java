e215120c5b5430383adceacc1f43f008ba217cd0
munsu_Lee <moonsu1016@naver.com>
2022-01-17 22:41:07 +0900

    Study about variables like double, float, bar etc..

---
+++ JAVA STUDY/Chapter2/src/binary/DoubleEx.java
@@ -0,0 +1,15 @@
+package binary;
+
+public class DoubleEx {
+
+	public static void main(String[] args) {
+		double num = 3.14;
+		var dNum = 3.14;
+		float fNum = 3.14f;
+		
+		System.out.println(num);
+		System.out.println(dNum);
+		System.out.println(fNum);
+	}
+
+}
